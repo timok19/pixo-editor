4f1eb34b89e72761a6838cae1ad00973b6a9db93
Temirkhan Amanzhanov <tima1199@gmail.com>
2022-11-14 00:20:24 +0100

    Added cropper feature

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { LoadingController } from '@ionic/angular';
+import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  croppedImage: any = '';
+  myImage = null;
 
-  constructor() {}
+  constructor(private loadingCtrl: LoadingController) {}
 
+  // select image from device
+  async selectImage() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      resultType: CameraResultType.Base64,
+    });
+
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    this.myImage = `data:image/jpeg;base64,${image.base64String}`;
+  }
+  imageLoaded() {
+    this.loadingCtrl.dismiss();
+  }
+
+  loadImageFailed() {
+    console.log('Image load failed');
+  }
 }
